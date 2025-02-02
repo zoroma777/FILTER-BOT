3dabe08cf1a191dcd9d43e19617ebc28d922a969
zoroma777 <ronozoro814@gmail.com>
2025-02-02 11:50:34 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -137,7 +137,7 @@ async def start(client, message):
     if len(message.command) == 2 and message.command[1] in ["subscribe", "error", "okay", "help"]:
         if PREMIUM_AND_REFERAL_MODE == True:
             buttons = [[
-                InlineKeyboardButton('⤬ Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴘ ⤬', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
+                InlineKeyboardButton('', url=f'')
             ],[
                 InlineKeyboardButton('', callback_data="shortlink_info"),
                 InlineKeyboardButton('', url=GRP_LNK)
@@ -151,7 +151,7 @@ async def start(client, message):
             ]]
         else:
             buttons = [[
-                InlineKeyboardButton('⤬ Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴘ ⤬', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
+                InlineKeyboardButton('', url=f'')
             ],[
                 InlineKeyboardButton('', callback_data="shortlink_info"),
                 InlineKeyboardButton('', url=GRP_LNK)
@@ -192,7 +192,7 @@ async def start(client, message):
         else:
             if PREMIUM_AND_REFERAL_MODE == True:
                 buttons = [[
-                    InlineKeyboardButton('⤬ Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴘ ⤬', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
+                    InlineKeyboardButton('', url=f'')
                 ],[
                     InlineKeyboardButton('', callback_data="shortlink_info"),
                     InlineKeyboardButton('', url=GRP_LNK)
@@ -206,7 +206,7 @@ async def start(client, message):
                 ]]
             else:
                 buttons = [[
-                    InlineKeyboardButton('⤬ Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴘ ⤬', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
+                    InlineKeyboardButton('', url=f'')
                 ],[
                     InlineKeyboardButton('', callback_data="shortlink_info"),
                     InlineKeyboardButton('', url=GRP_LNK)
